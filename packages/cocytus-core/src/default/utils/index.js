8c8af8cf17de27d60ba7ed265f1db2d59a4c1560
Jikkai Xiao <jikkai@users.noreply.github.com>
2019-02-21 16:32:09 +0900

    [release] v0.1.0 (#2)
    * feat: support theme selector
    
    * fix: fix npm scripts

--- packages/cocytus-core/src/default/utils/index.js
+++ packages/cocytus-core/src/default/utils/index.js
@@ -1,7 +1,7 @@
 export const decode = text => new TextDecoder('utf-8').decode(text)
 
 export const fetchFile = filename => {
-  return fetch(`./${filename}`)
+  return window.fetch(`./${filename}`)
     .then(resp => {
       const reader = resp.body.getReader()
       return reader.read()
