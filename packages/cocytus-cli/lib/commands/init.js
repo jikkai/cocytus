1d7fac00b337c73e67e80231283e3bc0095d25cf
Jikkai Xiao <sonne@asaki.me>
2019-02-21 15:25:00 +0800

    feat: support theme selector

--- packages/cocytus-cli/lib/commands/init.js
+++ packages/cocytus-cli/lib/commands/init.js
@@ -1,8 +1,9 @@
 const fs = require('fs-extra')
 const consola = require('consola')
+const inquirer = require('inquirer')
 const utils = require('../../utils')
 
-module.exports = target => {
+module.exports = async target => {
   const dest = utils.path.cwd(target)
 
   consola.start('Creating...')
@@ -10,9 +11,23 @@ module.exports = target => {
     fs.mkdirSync(dest)
   }
 
-  const src = utils.path.resolve(__dirname, '../../themes/default')
-  fs.copy(src, dest)
-    .then(() => {
-      consola.success('Done!')
-    })
+  const { theme } = await inquirer.prompt([{
+    name: 'theme',
+    type: 'list',
+    message: 'Please Select a theme:',
+    choices: [
+      { name: 'Default', value: 'default' }
+    ]
+  }])
+
+  const src = utils.path.resolve(__dirname, '../../template')
+
+  fs.copySync(src, dest)
+
+  const indexPath = `${dest}/index.html`
+  const outputIndex = fs.readFileSync(indexPath).toString()
+  const resultIndex = outputIndex.replace(/{{theme}}/g, theme)
+  fs.writeFileSync(indexPath, resultIndex)
+
+  consola.success('Done!')
 }
